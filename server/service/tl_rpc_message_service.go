8cc50068dafe1871c57e6f8025ef98e6f8ceb998
rk <r@gmail.com>
2018-11-08 22:53:18 +0800

    fix

--- server/service/tl_rpc_message_service.go
+++ server/service/tl_rpc_message_service.go
@@ -32,14 +32,16 @@ func (s *TLService) TLRpcMessageProcess(csess *ClientSession, msgId int64, seqNo
 
 	// rpc invoke
 	switch tlobj.(type) {
+
+	// help
 	case *mtproto.TL_help_getConfig:
 		res, err = s.TL_help_getConfig_Process(csess, tlobj)
-	case *mtproto.TL_auth_logOut:
-		res, err = s.TL_auth_logOut_Process(csess, tlobj)
-	case *mtproto.TL_langpack_getLangPack:
-		res, err = s.TL_langpack_getLangPack_Process(csess, tlobj)
 	case *mtproto.TL_help_getNearestDc:
 		res, err = s.TL_help_getNearestDc_Process(csess, tlobj)
+
+	// auth
+	case *mtproto.TL_auth_logOut:
+		res, err = s.TL_auth_logOut_Process(csess, tlobj)
 	case *mtproto.TL_auth_checkPhone:
 		res, err = s.TL_auth_checkPhone_Process(csess, tlobj)
 	case *mtproto.TL_auth_sendCode:
@@ -48,14 +50,30 @@ func (s *TLService) TLRpcMessageProcess(csess *ClientSession, msgId int64, seqNo
 		res, err = s.TL_auth_resendCode_Process(csess, tlobj)
 	case *mtproto.TL_auth_signIn:
 		res, err = s.TL_auth_signIn_Process(csess, tlobj)
-	case *mtproto.TL_messages_getDialogs:
-		res, err = s.TL_messages_getDialogs_Process(csess, tlobj)
+
+	// account
 	case *mtproto.TL_account_updateStatus:
 		res, err = s.TL_account_updateStatus_Process(csess, tlobj)
-	case *mtproto.TL_users_getFullUser:
-		res, err = s.TL_users_getFullUser_Process(csess, tlobj)
+
+	// contacts
+	case *mtproto.TL_contacts_importContacts:
+		res, err = s.TL_account_updateStatus_Process(csess, tlobj)
+	case *mtproto.TL_contacts_getContacts:
+		res, err = s.TL_contacts_getContacts_Process(csess, tlobj)
+
+	// langpack
+	case *mtproto.TL_langpack_getLangPack:
+		res, err = s.TL_langpack_getLangPack_Process(csess, tlobj)
+
+	// messages
+	case *mtproto.TL_messages_getDialogs:
+		res, err = s.TL_messages_getDialogs_Process(csess, tlobj)
 	case *mtproto.TL_messages_getPinnedDialogs:
 		res, err = s.TL_messages_getPinnedDialogs_Process(csess, tlobj)
+
+	// users
+	case *mtproto.TL_users_getFullUser:
+		res, err = s.TL_users_getFullUser_Process(csess, tlobj)
 	default:
 		Log.Debugf("havent implemented yet, TLType = %T", tlobj)
 		err = fmt.Errorf("havent implemented yet, TLType = %T", tlobj)
@@ -389,4 +407,28 @@ func (s *TLService) TL_messages_getPinnedDialogs_Process(csess *ClientSession, o
 	fulluser := &mtproto.TL_userFull{}
 
 	return fulluser, nil
+}
+
+// TL_contacts_importContacts
+func (s *TLService) TL_contacts_importContacts_Process(csess *ClientSession, object mtproto.TLObject) (mtproto.TLObject, error) {
+	Log.Infof("entering... client sessid = %v", csess.ClientSessionID)
+
+	// tlobj := object
+	// tl := tlobj.(*mtproto.TL_account_updateStatus)
+
+	// Log.Infof("TL_account_updateStatus = %+v", FormatStruct(tl))
+
+	return mtproto.ToBool(true), nil
+}
+
+// TL_contacts_getContacts
+func (s *TLService) TL_contacts_getContacts_Process(csess *ClientSession, object mtproto.TLObject) (mtproto.TLObject, error) {
+	Log.Infof("entering... client sessid = %v", csess.ClientSessionID)
+
+	// tlobj := object
+	// tl := tlobj.(*mtproto.TL_account_updateStatus)
+
+	// Log.Infof("TL_account_updateStatus = %+v", FormatStruct(tl))
+
+	return mtproto.ToBool(true), nil
 }
